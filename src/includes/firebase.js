d418d6189633de5d2efbe3b30adad3ecb376e637
khafidhasukma <khafidhasukma@gmail.com>
2022-10-06 23:07:09 +0700

    uploading song

--- src/includes/firebase.js
+++ src/includes/firebase.js
@@ -1,6 +1,7 @@
 import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
+import "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCVZiC05IuwfOUPjWUXOFZI38Fqws1mSW4",
@@ -15,6 +16,9 @@ firebase.initializeApp(firebaseConfig);
 
 const auth = firebase.auth();
 const db = firebase.firestore();
-const userCollection = db.collection("users");
+const storage = firebase.storage();
 
-export { auth, db, userCollection };
+const userCollection = db.collection("users");
+const songsCollection = db.collection("songs");
+
+export { auth, db, userCollection, songsCollection, storage };
