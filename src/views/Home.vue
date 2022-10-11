841a9dfa764b937f1174f96aa0386e6aacb77e97
khafidhasukma <khafidhasukma@gmail.com>
2022-10-11 22:02:03 +0700

    playing music

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,32 +1,38 @@
 <template>
-  <!-- Introduction -->
-  <section class="mb-8 py-20 text-white text-center relative">
-    <div
-      class="absolute inset-0 w-full h-full bg-cover introduction-bg"
-      style="background-image: url(assets/img/header.png)"
-    ></div>
-    <div class="container mx-auto">
-      <div class="text-white main-header-content">
-        <h1 class="font-bold text-5xl mb-5">Listen to Great Music!</h1>
+  <main>
+    <!-- Introduction -->
+    <section class="mb-8 py-20 text-white text-center relative">
+      <div
+        class="absolute inset-0 w-full h-full bg-cover introduction-bg"
+        style="background-image: url(assets/img/header.png)"
+      ></div>
+      <div class="container mx-auto">
+        <div class="text-white main-header-content">
+          <h1 class="font-bold text-5xl mb-5">Listen to Great Music!</h1>
+        </div>
       </div>
-    </div>
-  </section>
+    </section>
 
-  <!-- Main Content -->
-  <section class="container mx-auto">
-    <div class="bg-white rounded border border-gray-200 relative flex flex-col">
-      <div class="px-6 pt-6 pb-5 font-bold border-b border-gray-200">
-        <span class="card-title">Songs</span>
-        <!-- Icon -->
-        <i class="fa fa-headphones-alt float-right text-green-400 text-xl"></i>
+    <!-- Main Content -->
+    <section class="container mx-auto">
+      <div
+        class="bg-white rounded border border-gray-200 relative flex flex-col"
+      >
+        <div class="px-6 pt-6 pb-5 font-bold border-b border-gray-200">
+          <span class="card-title">Songs</span>
+          <!-- Icon -->
+          <i
+            class="fa fa-headphones-alt float-right text-green-400 text-xl"
+          ></i>
+        </div>
+        <!-- Playlist -->
+        <ol id="playlist">
+          <app-song-item v-for="song in songs" :key="song.docID" :song="song" />
+        </ol>
+        <!-- .. end Playlist -->
       </div>
-      <!-- Playlist -->
-      <ol id="playlist">
-        <app-song-item v-for="song in songs" :key="song.docID" :song="song" />
-      </ol>
-      <!-- .. end Playlist -->
-    </div>
-  </section>
+    </section>
+  </main>
 </template>
 
 <script>
