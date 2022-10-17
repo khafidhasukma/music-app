6c9ddc6c177e503e3db68dab1ab334d873b8f3a9
khafidhasukma <khafidhasukma@gmail.com>
2022-10-17 15:00:26 +0700

    performance optimizations

--- src/components/Upload.vue
+++ src/components/Upload.vue
@@ -65,6 +65,18 @@ export default {
           return;
         }
 
+        if (!navigator.onLine) {
+          this.uploads.push({
+            task: {},
+            current_progress: 100,
+            name: file.name,
+            variant: "bg-red-400",
+            icons: "fas fa-times",
+            text_class: "text-red-400",
+          });
+          return;
+        }
+
         const storageRef = storage.ref(); //music-3d6d6.appspot.com
         const songsRef = storageRef.child(`songs/${file.name}`); // music-3d6d6.appspot.com/songs/example.mp3
         const task = songsRef.put(file);
--- src/main.js
+++ src/main.js
@@ -7,9 +7,16 @@ import VeeValidatePlugnin from "./includes/validation";
 import { auth } from "./includes/firebase.js";
 import Icon from "./directives/icon";
 import i18n from "./includes/i18n";
+import { registerSW } from "virtual:pwa-register";
+import GlobalComponents from "./includes/_globals";
+import progressBar from "./includes/progress-bar";
 
 import "./assets/base.css";
 import "./assets/main.css";
+import "nprogress/nprogress.css";
+
+registerSW({ immediate: true });
+progressBar(router);
 
 let app;
 auth.onAuthStateChanged(() => {
@@ -20,6 +27,7 @@ auth.onAuthStateChanged(() => {
     app.use(router);
     app.use(VeeValidatePlugnin);
     app.use(i18n);
+    app.use(GlobalComponents);
     app.directive("icon", Icon);
 
     app.mount("#app");
